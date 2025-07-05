3ef827c577e286170ff05fa46fe9f94675690c73
Jean-Simon Lhost <jean.si@hotmail.fr>
2025-07-05 19:12:19 +0200

    Nettoyage du script

--- titanic.py
+++ titanic.py
@@ -1,34 +1,23 @@
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-import multiprocessing
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.preprocessing import OneHotEncoder, MinMaxScaler
 from sklearn.model_selection import train_test_split
-import pathlib
-from sklearn.preprocessing import MinMaxScaler
 from sklearn.ensemble import RandomForestClassifier
-import time
-import os
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.metrics import confusion_matrix
 
-# os.chdir('/home/coder/work/ensae-reproductibilite-application')
+
 TrainingData = pd.read_csv("data.csv")
-
-TrainingData.head()
-
-
-TrainingData["Ticket"].str.split("/").str.len()
-
-TrainingData["Name"].str.split(",").str.len()
+# TrainingData["Ticket"].str.split("/").str.len()
+# TrainingData["Name"].str.split(",").str.len()
 
 n_trees = 20
 max_depth = None
 max_features = "sqrt"
 
-TrainingData.isnull().sum()
-
-
-## Un peu d'exploration et de feature engineering
 
 ### Statut socioéconomique
 
@@ -51,11 +40,6 @@ sns.histplot(data=TrainingData, x="Age", bins=15, kde=False).set_title(
 plt.show()
 
 ## Encoder les données imputées ou transformées.
-from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.impute import SimpleImputer
-from sklearn.pipeline import Pipeline
-from sklearn.compose import ColumnTransformer
 
 numeric_features = ["Age", "Fare"]
 categorical_features = ["Embarked", "Sex"]
@@ -73,7 +57,6 @@ categorical_transformer = Pipeline(
         ("onehot", OneHotEncoder()),
     ]
 )
-
 
 preprocessor = ColumnTransformer(
     transformers=[
@@ -98,34 +81,24 @@ pipe = Pipeline(
 y = TrainingData["Survived"]
 X = TrainingData.drop("Survived", axis="columns")
 
-# On _split_ notre _dataset_ d'apprentisage pour faire de la validation croisée une partie pour apprendre une partie pour regarder le score.
+# On _split_ notre _dataset_ d'apprentisage pour faire de la validation croisée
 # Prenons arbitrairement 10% du dataset en test et 90% pour l'apprentissage.
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
 pd.concat([X_train, y_train], axis=1).to_csv("train.csv")
 pd.concat([X_test, y_test], axis=1).to_csv("test.csv")
 
-jetonapi = "$trotskitueleski1917"
+jeton_api = "$trotskitueleski1917"
 
 
 # Random Forest
-
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-from sklearn.model_selection import train_test_split
-import pathlib
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.ensemble import RandomForestClassifier
-
-
-# Ici demandons d'avoir 20 arbres
 pipe.fit(X_train, y_train)
 
 
-# calculons le score sur le dataset d'apprentissage et sur le dataset de test (10% du dataset d'apprentissage mis de côté)
+# calculons le score sur le train et sur le test
 # le score étant le nombre de bonne prédiction
 rdmf_score = pipe.score(X_test, y_test)
 rdmf_score_tr = pipe.score(X_train, y_train)
 print(f"{rdmf_score:.1%} de bonnes réponses sur les données de test pour validation")
-from sklearn.metrics import confusion_matrix
 
 print(20 * "-")
 print("matrice de confusion")
